1a9034356d58c6daf7bc379147b200f213e5e820
MINHA MARIYAM C P <minhamariyamcp@gmail.com>
2026-02-26 20:04:05 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,1 +1,17 @@
+from ultralytics import YOLO
+import cv2
 
+# Load trained YOLO26 segmentation model
+model = YOLO("weights/best.pt")   # path to trained weights
+
+# Image for prediction
+image_path = "test.jpg"
+
+# Run inference
+results = model.predict(
+    source=image_path,
+    save=True,
+    conf=0.25
+)
+
+print("✅ Detection completed. Check outputs folder.")
